e973b0819f09a96fda33f081d00f017bdbbb9855
pavanpasila <65785317+pavanpasila@users.noreply.github.com>
2021-10-19 11:40:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,16 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-pavan123"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
   # The configuration for the `remote` backend.
    terraform {
        backend "remote" {
-#         # The name of your Terraform Cloud organization.
+         # The name of your Terraform Cloud organization.
          organization = "Test_org_pavan"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+     
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
            name = "example-workspace"
       }
        }
      }
-#
-#     # An example resource that does nothing.
+    # An example resource that does nothing.
      resource "null_resource" "example" {
        triggers = {
          value = "A example resource that does nothing!"
